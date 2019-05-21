651fad779c7e8f348fb1df5172a946fa3ea87642
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-21 03:51:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,22 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-<<<<<<< HEAD
   const result = [`${element}`, ...array];
   return result;
 } 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var modify = array.unshift(element);
-=======
-  var array = [1];
-  array.unshift("foo")
+  var answer = array.unshift(element);
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = ['foo', 1]
->>>>>>> d599fb89ba3eb2e5f590433d2903a8a9ef37226e
-  return array;
-}
 
